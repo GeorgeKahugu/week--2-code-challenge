1cb28f86c1e10934a48ac63be2e4b96380bd0332
GeorgeKahugu <gee.kahugu@gmail.com>
2024-04-01 12:23:35 +0300

    twonumberstogenerateanarray

---
+++ week2-challenges/twonumberstogenerateanarray.js
@@ -0,0 +1,29 @@
+num1=2
+num2=8
+num3=-2
+
+// call a function
+function createArray(begin,end){
+        let array=[];
+        if(begin<=end){
+            for (let i=begin;i<=end; i++ ){
+                array.push(i);
+            }
+// else 
+        }else{
+            for (let i=begin; i = end; i--){
+                array.push(i);
+            }
+        }
+//return 
+        return array;
+    }
+// output
+   let output1=createArray(num1,num2)
+   let  output2=createArray(num3,num2)
+
+    console.log(output2)
+
+
+
+
